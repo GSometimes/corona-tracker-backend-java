822957dd85f91ebd1dd4c9dbfd57fdcc7ef24a2e
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-11-18 18:05:55 -0800

    commit to heroku

--- src/main/java/com/example/goresometimes/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/example/goresometimes/coronavirustracker/controllers/HomeController.java
@@ -5,10 +5,7 @@ import com.example.goresometimes.coronavirustracker.services.CoronavirusDataServ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,6 +27,6 @@ public class HomeController {
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
 
-        return  allStats; // Return template value
+        return  allStats;
     }
 }
